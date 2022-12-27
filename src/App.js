f26208e2939219fc1523f86b3805ec23e69ab3c5
Artem Romanov <artem.romanov@abz.agency>
2022-12-27 16:41:03 +0200

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const inputRef = useRef();
+  const [text, setText] = useState();
+  const [textState, setTextState] = useState();
+  useEffect(() => {
+    // if ('OTPCredential' in window) {
+      const ac = new AbortController();
+      setTextState('INit');
+      navigator.credentials.get({
+        otp: { transport:['sms'] },
+        signal: ac.signal
+      }).then((otp) => {
+        setTextState('INit OTP');
+        setText(otp.code);
+        inputRef.current.value = otp.code;
+        // if (form) form.submit();
+      }).catch((err) => {
+        console.error(err);
+      });
+    // }
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +30,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <p style={{color: 'white', minHeight: 40}}>{textState}</p>
+          <p style={{color: 'white', minHeight: 40}}>{text}</p>
+          <form>
+            <input onChange={e => setText(e.currentTarget.value)} ref={inputRef}/>
+          </form>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
